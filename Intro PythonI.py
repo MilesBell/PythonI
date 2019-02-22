9d7fd1576574156fb92e197935dcf0c636d7cc2f
MilesBell <46505841+MilesBell@users.noreply.github.com>
2019-02-21 19:02:55 -0800

    Add files via upload

---
+++ Intro PythonI.py
@@ -0,0 +1,91 @@
+#Dev: Miles Bell
+#Date: 02/21/2019
+#Desc: My first code using Python
+
+#This is how I print Hello World
+print("Hello World!")
+
+#Adding two numbers
+2+3
+
+#Print
+print("my name is Miles")
+print("I'm a python programmer :)")
+
+
+print("my name is 'Miles'.")
+print("Miles","BUAN","2019")
+
+print("my dad\'s name is LeRoy")
+print("my name is \n Miles and my dad\'s name is LeRoy")
+
+print("I'm born in "+"1998 "+ "on March 30th")
+print("my dad's name is LeRoy \n"*10)
+
+#versus (repeats "my dads name is LeRoy" 10 times each on a new line, versus just consecutive)
+
+print("I'm born in "+"1998 "+ "on March 30th")
+print("my dad's name is LeRoy "*10)
+
+print("Miles", "LeRoy", "Koren", "Danielle", "Nicole")
+print("MKTG 3000", "IS 3150", "UCOR 3100", "w/scores", ((4.0*4.0)/4.0), 4.0+4.0+4.0+4.0)
+
+#modulus
+print(2%5)
+print(6%2)
+
+name="John"
+print("Hello", name)
+
+name=input("Hi. What is your name?")
+print(name)
+print("Hi", name)
+input("\nPress the enter key to exit.")
+
+#^ the code is asking for the name as an input, then it prints the name, says "hi" t the name, then you can hit enter to exit (one line down with \n)
+
+#BTW, string manipulation i.e. (lower()) or (upper()) or (title()) or change part inside string (replace()), swapcase() which switches the case of each letter, strip(), replace(old,new[,max])
+
+chapter="Introduction to Python"
+print("original chapter name:")
+print(chapter)
+
+print("\nIn uppercase:")
+Print(chapter.upper())
+
+Print("\n\nPress the enter key to exit.")
+
+Chapter="Introduction to Python"
+print("original chapter name:")
+print(chapter)
+
+Print("\nIn uppercase:")
+Print(chapter.upper())
+
+Print("\n\nPress the enter key to exit.")
+
+print("my expenses are:")
+car=int(input("insurance:"))
+gas=int(input("gas for my car:"))
+rent=int(input("my rent is:"))
+food=int(input("groceries and dining out:"))
+shopping=int(input("my personal shopping"))
+total=car+gas+rent+food+shopping
+print("\nGrand total is:", total)
+
+#i.e.: ur gas expenses during a year: gas=gas*12 equivalent to gas*=12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
